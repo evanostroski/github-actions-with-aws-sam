84e77595d3215ddc9275d988fa3084b98a49289d
Evan Ostroski <eostroski@gmail.com>
2024-01-27 10:31:17 -0500

    clean up failed stack

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello evan!!",
+            "message": "one more time!",
             # "location": ip.text.replace("\n", "")
         }),
     }
