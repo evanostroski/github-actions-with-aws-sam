a19ad804efb304d2dd583b0b51865c897dd0cccc
Evan Ostroski <eostroski@gmail.com>
2024-02-02 10:48:53 -0500

    Bump

--- embedding/app.py
+++ embedding/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "one more time!",
+            "message": "embed!",
             "location": ip.text.replace("\n", "")
         }),
     }
